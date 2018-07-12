5d0ec298b0fb435778693dc6d7ff30162013bcd9
TinyD <ningkid345@163.com>
2018-07-12 22:27:37 +0800

    lodash

--- miao/lodash/tinyd0409-lodash.js
+++ miao/lodash/tinyd0409-lodash.js
@@ -1,4 +1,5 @@
 var tinyd0409 = {
+  //Array
   chunk: function(array, size) {
     var result = []
     for (j = 0;j<array.length;j+=size){
@@ -15,7 +16,7 @@ var tinyd0409 = {
   },
   compact: function(array){
     for(var i = 0; i < array.length ;i++){
-      if(array[i] == undefined||array[i] == null||array[i] == true ||array[i] == false){
+      if(array[i] == undefined||array[i] == null||array[i] == true ||array[i] == false ||array[i] == NaN){
         array.splice(i,1)
       }
     }
@@ -24,8 +25,45 @@ var tinyd0409 = {
   concat: function(array, [values]){
     
   },
+  difference: function(array, values){
+    var map = {}
+    for (var i = 0;i < values.length ; i++){
+      if(values[i] in map){
+      }else{
+        map[values[i]] = true
+      }
+    }
+    for(var j = 0;j < array.length ; j++){
+      if(array[j] in map){
+        array.splice(j,1)
+      }
+    }
+    return array
+  };
+  join: function(array, separator){
+    var result = ""  
+    if(separator == undefined){
+      separator = ","
+    }
+    for (var i = 0;i < array.length; i++){
+      result = result + separator + array[i]
+    }
+    return result
+  };
+  last: function(array){
+    return array[array.length-1]
+  }
+//Math
+  add: function(augend, addend){
+    return augend + addend
+  };
 
+  ceil: function(number, precision){
 
+  };
+  divide: function(dividend, divisor){
+    return dividend/divisor
+  }
 
 
 
